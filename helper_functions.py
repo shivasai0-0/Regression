09c0ddd667a6fb1af84e47118669d8f5a60bebb3
shivasai0-0 <alladishivasai15@gmail.com>
2025-08-06 21:29:49 +0530

    explored data

---
+++ helper_functions.py
@@ -0,0 +1,16 @@
+def Stats_fn(Stats:pd.DataFrame)->pd.DataFrame:
+    """ Stats:df.describe
+        skewness->absolute skewness>1
+        IQR/std-> 1< and 3>
+        outliers->(max-min)/std >6
+        long left tail or long right tail->(min,max)/mean>2
+        max outliers->Q3+1.5*IQR<max
+        min outliers->Q1-1.5*IQR>min     """
+    IQR=Stats.iloc[6]-Stats.iloc[4]
+    return pd.DataFrame({"skewness":df.drop("ocean_proximity",axis=1,inplace=False).skew().abs()>1,
+        "IQR/std":(IQR/Stats.loc["std"]).between(1,3,inclusive="neither"),
+        "outliers":(Stats.iloc[7]-Stats.iloc[3])/Stats.iloc[2]>6,
+        "long right tail":(Stats.loc["max"]/Stats.loc["mean"])>2,
+        "long left tail":(Stats.loc["min"]/Stats.loc["mean"])>2,
+        "max outliers":(Stats.iloc[6]+1.5*IQR)<Stats.loc["max"],
+        "min outliers":Stats.iloc[4]-1.5*(IQR)>Stats.loc["min"]})
