0d49422e3e3a4f819d4a2b7d0d760437dae675fd
shivasai0-0 <alladishivasai15@gmail.com>
2025-08-07 19:11:35 +0530

    done with eda with minimal plotting

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,6 @@
-def Stats_fn(Stats:pd.DataFrame)->pd.DataFrame:
+import pandas as pd
+
+def Stats_fn(df:pd.DataFrame)->pd.DataFrame:
     """ Stats:df.describe
         skewness->absolute skewness>1
         IQR/std-> 1< and 3>
@@ -6,8 +8,10 @@ def Stats_fn(Stats:pd.DataFrame)->pd.DataFrame:
         long left tail or long right tail->(min,max)/mean>2
         max outliers->Q3+1.5*IQR<max
         min outliers->Q1-1.5*IQR>min     """
+    df=df.select_dtypes(include="number")
+    Stats=df.describe()
     IQR=Stats.iloc[6]-Stats.iloc[4]
-    return pd.DataFrame({"skewness":df.drop("ocean_proximity",axis=1,inplace=False).skew().abs()>1,
+    return pd.DataFrame({"skewness":df.skew().abs()>1,
         "IQR/std":(IQR/Stats.loc["std"]).between(1,3,inclusive="neither"),
         "outliers":(Stats.iloc[7]-Stats.iloc[3])/Stats.iloc[2]>6,
         "long right tail":(Stats.loc["max"]/Stats.loc["mean"])>2,
